3b83720af6738f04d7754ceed65cad388601a416
Kate Jarvis <katejarvis1@hotmail.com>
2023-09-15 16:16:56 -0400

    Camera Rotation
    Camera rotates around player character when using Q and E

---
+++ Automaton/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float rotateSpeed;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateCamera();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+      
+       
+    }
+
+    public void RotateCamera() 
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.RotateAround(target.position, transform.up, rotateSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.RotateAround(target.position, transform.up, rotateSpeed * Time.deltaTime);
+        }
+    }
+}
