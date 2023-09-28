6a6f7db448ffd0ff314410cfd546de9cf645b321
Kate Jarvis <katejarvis1@hotmail.com>
2023-09-27 20:06:23 -0400

    Removed Movement
    Cleared out movement code for complete rehaul

--- Automaton/Assets/Scripts/Player/PlayerController.cs
+++ Automaton/Assets/Scripts/Player/PlayerController.cs
@@ -12,13 +12,16 @@ public class PlayerController : MonoBehaviour
     public float deaccelerationRate;
     public float jumpForce;
     public float rotateSpeed;
+    public float dashSpeed;
+    public float dashTime;
 
     [Header("Ground Layer")]
     public LayerMask groundMask;
 
     [Header("Movement Checks")]
-    public bool isGrounded;
-    public bool isJumping;
+    public bool isMoving;
+
+    public Vector3 currentVelocity;
      
 
 
@@ -31,87 +34,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        //Check for player jump input
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            isJumping = true;
-        }
+        currentVelocity = _rb.velocity;
     }
 
     private void FixedUpdate()
     {
 
         Movement();
-
-
-        //Check if player is grounded
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.4f, groundMask))
-        {
-            Debug.DrawRay(transform.position, Vector3.down * 0.4f, Color.green);
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
-        }
-
-        if (isJumping)
-        {
-            Jump();
-        }
-        
+      
     }
+
+ 
 
     public void Movement() 
     {
-       
-        //Player movement and input
 
-        if (Input.GetKey(KeyCode.A) && isGrounded)
-        {
-            _rb.AddForce(Vector3.left * accelerationRate * Time.deltaTime, ForceMode.Impulse);
-        }
-        else if (Input.GetKey(KeyCode.D) && isGrounded)
-        {
-            _rb.AddForce(Vector3.right * accelerationRate * Time.deltaTime, ForceMode.Impulse);
-        }
-        else if (Input.GetKey(KeyCode.W) && isGrounded)
-        {
-            _rb.AddForce(Vector3.forward * accelerationRate * Time.deltaTime, ForceMode.Impulse);
-        }
-        else if (Input.GetKey(KeyCode.S) && isGrounded)
-        {
-            _rb.AddForce(-Vector3.forward * accelerationRate * Time.deltaTime, ForceMode.Impulse);
-        }
-        else 
-        {
-            if (_rb.velocity.x != 0)
-            {
-                _rb.AddForce(new Vector3(-_rb.velocity.x, 0, 0) * deaccelerationRate * Time.deltaTime, ForceMode.Impulse);
-            }
-            else if (_rb.velocity.z != 0)
-            {
-                _rb.AddForce(new Vector3(0, 0, -_rb.velocity.z) * deaccelerationRate * Time.deltaTime, ForceMode.Impulse);
-            }
-        }
-
-        if (Input.GetKey(KeyCode.Q))
-        {
-            transform.RotateAround(this.transform.position, transform.up, rotateSpeed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.E))
-        {
-            transform.RotateAround(this.transform.position, transform.up, -rotateSpeed * Time.deltaTime);
-        }
 
     }
 
-    public void Jump() 
-    {
-        _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        isJumping = false;
-    }
+
 }
