700aa207dce0937d755e112e32e5d75636e11a6c
Kate Jarvis <katejarvis1@hotmail.com>
2023-09-27 21:58:50 -0400

    New Movement and Dashing
    Added new movement code
    Added Dash System with cooldown

--- Automaton/Assets/Scripts/Player/PlayerController.cs
+++ Automaton/Assets/Scripts/Player/PlayerController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rb;
@@ -13,21 +15,27 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float rotateSpeed;
     public float dashSpeed;
-    public float dashTime;
+    public float currentDashTime;
+    public float dashCoolDownTime;
+
 
     [Header("Ground Layer")]
     public LayerMask groundMask;
 
     [Header("Movement Checks")]
     public bool isMoving;
+    public bool canDash;
 
     public Vector3 currentVelocity;
+
+    private Vector3 moveDir;
      
 
 
     // Start is called before the first frame update
     void Start()
     {
+       
         _rb = GetComponent<Rigidbody>();
     }
 
@@ -35,22 +43,53 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         currentVelocity = _rb.velocity;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        moveDir = new Vector3(x, 0, z);
+        _rb.velocity = moveDir * accelerationRate;
+
+        if (Input.GetKeyUp(KeyCode.Space) && canDash)
+        {
+            
+            Dash();
+           
+        }
+
+        if (currentDashTime > 0)
+        {
+            currentDashTime -= Time.deltaTime;
+        }
+        else if (currentDashTime <= 0)
+        {
+            canDash = true;
+        }
+       
     }
 
     private void FixedUpdate()
     {
 
-        Movement();
+      
       
     }
 
- 
+  
 
     public void Movement() 
     {
 
 
     }
+
+    public void Dash() 
+    {
+        _rb.AddForce(moveDir * dashSpeed, ForceMode.Impulse);
+        canDash = false;
+        currentDashTime = dashCoolDownTime;
+    }
+
 
 
 }
