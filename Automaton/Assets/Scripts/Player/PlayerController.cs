5c13889a520172209459ed02fc3931aedfd5c073
Kate Jarvis <katejarvis1@hotmail.com>
2023-10-02 15:47:59 -0400

    Player Rotation
    rotation based on mouse position

--- Automaton/Assets/Scripts/Player/PlayerController.cs
+++ Automaton/Assets/Scripts/Player/PlayerController.cs
@@ -9,12 +9,13 @@ public class PlayerController : MonoBehaviour
     private Rigidbody _rb;
     public GameObject Hitbox;
     private Collider hitbox;
-
+    public Camera cam;
 
     [Header("Movement Settings")]
 
     public float accelerationRate;
     public float deaccelerationRate;
+    public float rotationRate;
     public float jumpForce;
     public float rotateSpeed;
     public float dashSpeed;
@@ -68,46 +69,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-       
 
-
-        currentVelocity = _rb.velocity;
-
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        moveDir = new Vector3(x, 0, z);
-        moveDir.Normalize();
-       
-        _rb.velocity = moveDir * accelerationRate;
-
-        if (moveDir != Vector3.zero)
-        {
-            this.transform.forward = -moveDir;
-        }
-
-        if (moveDir == Vector3.back)
-        {
-            sprite.sprite = Forward;
-        }
-        else if (moveDir == Vector3.right)
-        {
-            sprite.sprite = Right;
-            sprite.flipX = false;
-        }
-        else if (moveDir == Vector3.left)
-        {
-            sprite.sprite = Right;
-            sprite.flipX = true;
-
-
-
-        }
-        else if (moveDir == Vector3.forward)
-        {
-            sprite.sprite = Back;
-        }
+        Movement();
+        LookatMouse();
+        RotateSprite();
 
         if (Input.GetKeyUp(KeyCode.Space) && canDash)
         {
@@ -153,9 +118,67 @@ public class PlayerController : MonoBehaviour
 
     public void Movement() 
     {
+        currentVelocity = _rb.velocity;
 
-    
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        moveDir = new Vector3(x, 0, z);
+        moveDir.Normalize();
+
+        _rb.velocity = moveDir * accelerationRate;
+
     }
+
+
+    public void LookatMouse() 
+    {
+        
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+      
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitdist;
+        RaycastHit hit;
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+            Quaternion targetRotation = Quaternion.LookRotation(-targetPoint - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation,targetRotation,rotateSpeed * Time.deltaTime);
+        }
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.point.x < this.transform.position.x)
+            {
+                sprite.sprite = Right;
+                sprite.flipX = true;
+
+            }
+            else if (hit.point.x > this.transform.position.x)
+            {
+                sprite.sprite = Right;
+                sprite.flipX = false;
+            }
+            else if (hit.point.y > this.transform.position.z)
+            {
+
+                sprite.sprite = Back;
+            }
+            else if (hit.point.y < this.transform.position.z)
+            {
+
+                sprite.sprite = Forward;
+            }
+
+        }
+    }
+
+    public void RotateSprite() 
+    {
+        var playerScreenPoint = Camera.main.WorldToScreenPoint(this.transform.position);
+
+    }
+
 
     public void MeleeAttack() 
     {
