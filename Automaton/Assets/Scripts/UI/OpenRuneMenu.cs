8de81f3dd9cd32f60fcffe5194050c84e4c6f30f
GrayAidan <114709817+GrayAidan@users.noreply.github.com>
2023-12-09 16:03:47 -0500

    stuff

--- Automaton/Assets/Scripts/UI/OpenRuneMenu.cs
+++ Automaton/Assets/Scripts/UI/OpenRuneMenu.cs
@@ -7,8 +7,15 @@ using UnityEngine.UI;
 public class OpenRuneMenu : MonoBehaviour
 {
     public PlayerAimer playerAimScript;
+    public ElementInfoDatabase EID;
     public TextMeshProUGUI spellText;
     public TextMeshProUGUI spellText2;
+
+    public List<GameObject> workBenches;
+
+    public float workBenchInteractionRange;
+    public LayerMask playerLayerMask;
+    public RectTransform interactionTextUI;
 
     // Start is called before the first frame update
     void Start()
@@ -19,68 +26,96 @@ public class OpenRuneMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (CheatCodes.CheatsOn)
+        {
+            RuneDebugChange();
+            print("bruh");
+        }
 
+        foreach(GameObject workbench in workBenches)
+        {
+            if (workbench.activeSelf)
+            {
+                Collider[] hitColls = Physics.OverlapSphere(workbench.transform.position, workBenchInteractionRange, playerLayerMask);
+                if(hitColls.Length > 0)
+                {
+                    foreach (Collider coll in hitColls)
+                    {
+                        interactionTextUI.position = new Vector3(workbench.transform.position.x, workbench.transform.position.y + 3, workbench.transform.position.z + 3);
+                    }
+                }
+                else if(hitColls.Length == 0)
+                {
+                    interactionTextUI.position = new Vector3(10000, 10000, 10000);
+                }
+            }
+        }
+    }
+
+    public void ChangeRune(int element,int rune)
+    {
+        playerAimScript.SetElement(element, rune);
+    }
+
+    public void RuneDebugChange()
+    {
+        int element = 1;
+        
+        if(Input.GetKey("left shift"))
+        {
+            element = 2;
+        }
+        
         if (Input.GetKeyDown("1"))
         {
-            ChangeRune(0);
-            
+            ChangeRune(element, 0);
+
         }
         else if (Input.GetKeyDown("2"))
         {
-            ChangeRune(1);
+            ChangeRune(element, 1);
 
         }
         else if (Input.GetKeyDown("3"))
         {
-            ChangeRune(2);
+            ChangeRune(element, 2);
 
         }
         else if (Input.GetKeyDown("4"))
         {
-            ChangeRune(3);
+            ChangeRune(element, 3);
 
         }
         else if (Input.GetKeyDown("5"))
         {
-            ChangeRune(4);
+            ChangeRune(element, 4);
 
         }
         else if (Input.GetKeyDown("6"))
         {
-            ChangeRune(5);
+            ChangeRune(element, 5);
 
         }
         else if (Input.GetKeyDown("7"))
         {
-            ChangeRune(6);
+            ChangeRune(element, 6);
 
         }
         else if (Input.GetKeyDown("8"))
         {
-            ChangeRune(7);
+            ChangeRune(element, 7);
 
         }
         else if (Input.GetKeyDown("9"))
         {
-            ChangeRune(8);
+            ChangeRune(element, 8);
 
         }
     }
 
-    public void ChangeRune(int rune)
+    private void OnDrawGizmos()
     {
-        if (Input.GetKey("left shift"))
-        {
-            playerAimScript.SetElement(2, rune);
-            spellText2.text = "Spell " + (rune+ 1).ToString();
-        }
-        else
-        {
-            playerAimScript.SetElement(1, rune);
-            spellText.text = "Spell " + (rune + 1).ToString();
-        }
-        
-
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(workBenches[0].transform.position, workBenchInteractionRange);
     }
 }
