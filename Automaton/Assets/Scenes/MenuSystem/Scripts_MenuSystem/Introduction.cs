ef60dd9657ae29dc75d16b0681870455d272bcc7
Tam <tamaraljubi2001@gmail.com>
2024-03-28 19:06:43 -0400

    Polishing Menu System
    List of changes:
    - Intro Menu Animation + Art update
    - Main Menu Animation + functionality and art update (no more separate settings scene)
    - Credits scene + animation
    - Pause Menu Update UI + animation

--- Automaton/Assets/Scenes/MenuSystem/Scripts_MenuSystem/Introduction.cs
+++ Automaton/Assets/Scenes/MenuSystem/Scripts_MenuSystem/Introduction.cs
@@ -5,13 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class Introduction : MonoBehaviour
 {
+    //set object reference
+    //public GameObject introScreen;
+    //public Animator anim;
     [SerializeField] private string MainMenuScene = "MainMenu";
 
     public void Update()
     {
+        SceneManager.LoadScene(MainMenuScene);
+        /*
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene(MainMenuScene);
+            //move object - moves it # of units per second
+            //Tam - tried physically moving with script but it's causing issues so imma try animation in unity
+            //introScreen.transform.position = new Vector3(0,2,0) * Time.deltaTime;
+            /*
+            anim.Play("IntroMove");
+            time = yield return new WaitForSeconds(5f);
+            if (time = 5f)
+            {
+                SceneManager.LoadScene(MainMenuScene);
+            }
+            
         }
+        */
     }
 }
--- Automaton/Assets/Scenes/MenuSystem/Scripts_MenuSystem/SettingsButton.cs
+++ Automaton/Assets/Scenes/MenuSystem/Scripts_MenuSystem/SettingsButton.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+//using UnityEngine.SceneManagement;
 
 public class SettingsButton : MonoBehaviour
 {
-    [SerializeField] private string MainMenuSettingsScene = "Settings";
+    private Animator mAnimator;
+    void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+    }
+    //[SerializeField] private string MainMenuSettingsScene = "Settings";
     public void MainSettingsLoader()
     {
-        SceneManager.LoadScene(MainMenuSettingsScene);
+        mAnimator.SetTrigger("SettingsButtonPressed"); //SceneManager.LoadScene(MainMenuSettingsScene);
     }
 }
